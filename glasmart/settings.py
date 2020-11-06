93763d4403dc286106fe41b2b6ccb431b1bf727b
albertogameros <c.alberto.gameros.b@gmail.com>
2020-11-05 19:16:22 -0700

    add localhost to alowed hosts

--- glasmart/settings.py
+++ glasmart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'admin123'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.69','189.237.215.110']
+ALLOWED_HOSTS = ['192.168.1.69','189.237.215.110','localhost']
 
 
 # Application definition
