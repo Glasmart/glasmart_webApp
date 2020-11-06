217e26362db1c3c5e6a1703b3ace0410e89cac7b
HaggarB <hector2383@gmail.com>
2020-11-05 21:00:33 -0700

    Despliegue de módulos en dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,28 +3,36 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 # Models
-from login.models import Profile
+from login.models import User
 from dashboard.models import Card
 # Create your views here.
 
 @login_required
 def home(request):
-    user = request.user
-    # uprofile = UserProfile.objects.get(id_user=user)
-    # print(uprofile)
-    # usercards = UserCard.objects.filter(id_user=user)
-    # print(usercards)
-    # return render(request,'dashboard/index.html',{'userCards':usercards,'uprofile':uprofile})
+    _user = User.objects.get(id=request.user.id)
+    user_cards= _user.profile.cards.all()
+    
+    return render(request,'dashboard/index.html',{'userCards':user_cards})
 
 @login_required
 def turn_card(request):
-
-    pass
+    
+    return render(request,'dashboard/index.html')
     
 
 @login_required
 def create_first_cards(request):
-    pass
-    
+    # UP = UserProfile.objects.get(id = request.POST['profile'])
+    # if UP.id_profile.name == "Administrador":
+    #     for card in Card.objects.all() :
+    #         UserCard(id_user=request.user, id_profile=UP.id_profile, id_card=card).save()
+    # if UP.id_profile.name == "Basico":
+    #     for card in Card.objects.all()[:2] :
+    #         UserCard(id_user=request.user, id_profile=UP.id_profile, id_card=card).save()
+    # if UP.id_profile.name == "Moderado":
+    #     for card in Card.objects.all()[:4] :
+    #         UserCard(id_user=request.user, id_profile=UP.id_profile, id_card=card).save()
+
+    return render(request,'dashboard/index.html')
 
 
