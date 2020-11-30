49249d5e051c0e81a20cbfb5c153ce06a249b25e
albertogameros <c.alberto.gameros.b@gmail.com>
2020-11-30 12:39:08 -0700

    Send new parametters

--- glasmart/settings.py
+++ glasmart/settings.py
@@ -31,17 +31,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
 
-# SECURE_HSTS_SECONDS = True
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_HSTS_PRELOAD = True
 
 # Application definition
 
