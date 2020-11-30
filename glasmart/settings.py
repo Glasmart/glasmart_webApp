2e1865baf6036af45ed3b8f6fa670c5fb007e781
albertogameros <c.alberto.gameros.b@gmail.com>
2020-11-30 02:43:32 -0700

    here we're going again

--- glasmart/settings.py
+++ glasmart/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',
+            str(BASE_DIR / 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
