62febaa88c9318a15cebb17dfe2f0e2df433e338
albertogameros <c.alberto.gameros.b@gmail.com>
2020-11-29 18:31:51 -0700

    Update settings.py

--- glasmart/settings.py
+++ glasmart/settings.py
@@ -60,7 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'glasmart.urls'
 
